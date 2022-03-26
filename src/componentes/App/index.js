aecbc89eba1b9fc0b30e28ce7de75c41ea7cfa03
Lucas Viana <lucasvfv@gmail.com>
2022-03-26 16:48:24 -0300

    feat: bonus botao de voltar adicionado

--- src/componentes/App/index.js
+++ src/componentes/App/index.js
@@ -1,9 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react"
-
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Filmes from ".././TelaFilmes/Filmes";
 import Assentos from "../TelaAssentos/Assentos";
-
 import Horarios from "../TelaHorario/Horarios";
 import Sucesso from "../TelaSucesso/Sucesso";
 import "./style.css"
@@ -13,20 +10,26 @@ import "./style.css"
 
 export default function App() {
 
+    const navigate = useNavigate()
 
-
+    const voltar =
+        <div onClick={() => navigate(-1)} >
+            <ion-icon name="chevron-back-outline">
+            </ion-icon>
+        </div>
 
     return (
-        <BrowserRouter>
+        <>
             <header className="App">
+                {window.location.pathname !== "/" ? voltar : ""}
                 <h1>CINEFLEX</h1>
             </header>
             <Routes>
                 <Route path="/" element={<Filmes />}></Route>
                 <Route path="/filme/:filmeId" element={<Horarios />}></Route>
-                <Route path="/sessao/:sessaoId" element={<Assentos/>}></Route>
+                <Route path="/sessao/:sessaoId" element={<Assentos />}></Route>
                 <Route path="/sucesso" element={<Sucesso />}></Route>
             </Routes>
-        </BrowserRouter>
+        </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import reactDom from "react-dom";
 import App from "./componentes/App/"
+import {BrowserRouter} from "react-router-dom"
 
 import "./assets/css/reset.css"
 import "./assets/css/style.css"
 
 
-reactDom.render(<App/>, document.querySelector('.root'))
+reactDom.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.querySelector('.root'))
